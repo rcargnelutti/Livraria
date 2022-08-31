aa5dbec556a7bc86b2e37873a698f94b16f6a86f
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-31 10:22:14 -0300

    [Django - DRF] - 7 - Categoria e Editora apresentadas no Admin do Django

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Categoria, Editora
+
+admin.site.register(Categoria)
+admin.site.register(Editora)
--- core/models.py
+++ core/models.py
@@ -3,8 +3,13 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.descricao
     
 class Editora(models.Model):
     nome = models.CharField(max_length=255)
     site = models.URLField()
-    
+
+    def __str__(self):
+        return self.nome
