0ea390ea5289b973f29c52fdcddccb812918d987
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-31 16:57:31 -0300

    18 - Métodos Post e Retrieve usando class-based views

--- core/views.py
+++ core/views.py
@@ -1,4 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from core.models import Categoria
+import json
 
-def teste(request):
-    return HttpResponse("Olá mundo do Django!")
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoriaView(View):
+    def get(self, request, id=None):
+        if id:
+            qs = Categoria.objects.get(id=id)
+            data = {}
+            data['id'] = qs.id
+            data['descricao'] = qs.descricao
+            return JsonResponse(data)
+        else: 
+            data = list(Categoria.objects.values())
+            formatted_data = json.dumps(data, ensure_ascii=False)
+            return HttpResponse(formatted_data, content_type="application/json")
+
+    def post(self, request):
+        json_data = json.loads(request.body)
+        nova_categoria = Categoria.objects.create(**json_data)
+        data = {"id": nova_categoria.id, "descricao": nova_categoria.descricao}
+        return JsonResponse(data)
--- livraria/urls.py
+++ livraria/urls.py
@@ -20,5 +20,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teste/', views.teste),
+    path('categorias/', views.CategoriaView.as_view()),
+    path('categorias/<int:id>/', views.CategoriaView.as_view()),
 ]
