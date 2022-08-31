a494d87f1fa2b67ce3f6259a73e842844ce019db
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-31 11:16:35 -0300

    9 - Model com ForeignKey - Livros com categoria e editora

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
-from core.models import Categoria, Editora, Autor
+from core.models import Categoria, Editora, Autor, Livro
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Livro)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'titulo', 'ISBN', 'quantidade', 'preco', 'editora']
+    list_display_links = ['id', 'titulo', 'ISBN']
+
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
+# admin.site.register(Livro)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from pyexpat import model
+from unicodedata import category
 from django.db import models
 
 class Categoria(models.Model):
@@ -22,3 +23,14 @@ class Autor(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.FloatField()
+    category = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+
+    def __str__(self):
+        return "%s (%s)" %(self.titulo, self.editora)
