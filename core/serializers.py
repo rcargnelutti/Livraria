efedd5a6cdbb17f7861f869908c4b6601272cfd8
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-02 08:32:59 -0300

    Ajustes na representação da listagem de Livros via serializer - parte 1

--- core/serializers.py
+++ core/serializers.py
@@ -24,3 +24,10 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
+
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,10 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
-from core.serializers import LivroSerializer
+from core.serializers import LivroSerializer, LivroDetailSerializer
 
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
+    # serializer_class = LivroSerializer
     # http_method_names = ['get', 'options', 'head', 'patch', 'post', 'delete']
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LivroDetailSerializer
+        if self.action == "retrive":
+            return LivroDetailSerializer
+        return LivroSerializer
