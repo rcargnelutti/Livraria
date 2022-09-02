79f0b7b2181fc2f3482c2a727d509846427eefa8
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-09-02 11:07:02 -0300

    34 - Endpoint para criar novas Compras (Many to Many Relationships)

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,10 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Compra
-from core.serializers import CompraSerializer
+from core.serializers import CompraSerializer, CriarEditarCompraSerializer
 
 
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
-    serializer_class = CompraSerializer
+    # serializer_class = CompraSerializer
     # http_method_names = ['get', 'options', 'head', 'patch', 'post', 'delete']
+
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return CompraSerializer
+        return CriarEditarCompraSerializer
